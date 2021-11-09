eef2c0814fcfa618f9771ffb906b051da33646ae
profitsw2000 <profitsw2000@gmail.com>
2021-11-09 22:03:25 +0300

    Created new class that will be get data from resources and provide it to application. Also added football clubs logos in drawable folder.

---
+++ app/src/main/java/ru/profitsw2000/recyclerviewdemo/AppData.java
@@ -0,0 +1,19 @@
+package ru.profitsw2000.recyclerviewdemo;
+
+import android.content.Context;
+
+import java.util.List;
+
+public class AppData {
+
+    private String clubName ;
+    private String clubCity ;
+    private String country  ;
+    private int logoID  ;
+
+    private Context context ;
+
+    private List<FootballClub> clubs    ;
+
+    private static AppData appData  ;
+}
