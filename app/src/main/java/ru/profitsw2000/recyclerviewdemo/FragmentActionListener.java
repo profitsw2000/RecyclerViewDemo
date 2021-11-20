d0c5581307a35e3860999bd5141c6c88148006e5
profitsw2000 <profitsw2000@gmail.com>
2021-11-20 21:09:51 +0300

    Interface FragmentActionListener.java was incorporated in project for interchanging of data between fragments.

---
+++ app/src/main/java/ru/profitsw2000/recyclerviewdemo/FragmentActionListener.java
@@ -0,0 +1,12 @@
+package ru.profitsw2000.recyclerviewdemo;
+
+import android.os.Bundle;
+
+public interface FragmentActionListener {
+
+    String ACTION_KEY = "action_key"    ;
+    int ACTION_VALUE_CLUB_SELECTED = 1  ;
+    String KEY_SELECTED_CLUB = "KEY_SELECTED_CLUB"    ;
+
+    void onActionPerformed(Bundle bundle)    ;
+}
